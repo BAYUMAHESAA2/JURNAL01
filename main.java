ad483296d8b059f5d113b37f0f06cf87cdafb966
BAYUMAHESAA2 <bayumahesa36@gmail.com>
2023-09-26 17:46:17 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main{
+    public static void main(String [] args){
+        System.out.println("Nama: Muhammad Bayu Mahesa");
+        System.out.println("Nim: 607062300017");
+        System.out.println("Kelas: D3IF-47-03");
+    }
+    
+}
